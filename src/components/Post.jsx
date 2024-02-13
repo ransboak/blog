3b03e8d8a7a0dacfa329167a04ac2d646a19b58d
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-13 03:30:46 -0800

    Updates

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom'
 
 const Post = ({id, title, thumbnail, desc, authorID}) => {
   return (
-    <div>
-        {id}
-    </div>
+    <Link to={`posts/${id}`} className='post'>
+        <img src={thumbnail} alt="" />
+        <h4>{title}</h4>
+        <p>{desc}</p>
+    </Link>
   )
 }
 
