1c0f07fd1bfc37a53374848aa8fa50cdbe2b4647
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-13 03:29:18 -0800

    Updates

--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -21,14 +21,42 @@ const dummyPosts = [
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
     authorID: 4,
   },{
-    id: 1,
+    id: 3,
     thumbnail : Thumbnail3,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
     category: 'education',
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
     authorID: 5,
   },{
-    id: 1,
+    id: 4,
+    thumbnail : Thumbnail4,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
+    category: 'education',
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
+    authorID: 6,
+  }, {
+    id: 5,
+    thumbnail : Thumbnail1,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
+    category: 'education',
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
+    authorID: 3,
+  },{
+    id: 6,
+    thumbnail : Thumbnail2,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
+    category: 'education',
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
+    authorID: 4,
+  },{
+    id: 7,
+    thumbnail : Thumbnail3,
+    title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
+    category: 'education',
+    desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
+    authorID: 5,
+  },{
+    id: 8,
     thumbnail : Thumbnail4,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
     category: 'education',
@@ -40,10 +68,10 @@ const dummyPosts = [
 const Posts = () => {
   return (
     <section>
-      <div className="container">
+      <div className="container posts">
         {
           dummyPosts.map(({id, title, thumbnail, desc, authorID}) => {
-            return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorId} />
+            return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} />
           })
         }
       </div>
