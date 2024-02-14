4323fe5005a4f46d810273d6b54781d0aa6697c8
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 02:55:07 -0800

    Updates

--- src/pages/PostDetail.jsx
+++ src/pages/PostDetail.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import PostAuthor from '../components/PostAuthor'
+import { Link } from 'react-router-dom'
 
 const PostDetail = () => {
   return (
     <section className='post-detail'>
-      <div className="container post__container">
-        Post
+      <div className="container post-detail__container">
+        <div className="post-detail__container">
+          <PostAuthor />
+          <div className="post-detail__buttons">
+            <Link to={`post/gdhd/edit`} className='btn sm primary'>edit</Link>
+            <Link to={`post/gdhd/delete`} className='btn sm danger'>delete</Link>
+          </div>
+        </div>
       </div>
     </section>
   )
