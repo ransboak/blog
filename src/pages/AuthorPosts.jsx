59ce0af8b9040da9e4e5908ad9a4663d9820b3da
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 05:34:35 -0800

    Updates

--- src/pages/AuthorPosts.jsx
+++ src/pages/AuthorPosts.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import dummyPosts from '../data'
+import Post from '../components/PostItem'
 
 const AuthorPosts = () => {
+  const [posts, setPosts] = useState(dummyPosts)
   return (
-   <section className='posts'>
-        {posts.length > 0 ? <div className="container posts__container">
-        {
-          posts.map(({id, title, thumbnail, desc, category, authorID}) => {
-            return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} category={category} />
-          })
-        }
-        </div> : <h2 className='center'>No posts yet</h2>} 
-    </section>
+    <section className='author__posts'>
+    {posts.length > 0 ? <div className="container posts__container">
+    {
+      posts.map(({id, title, thumbnail, desc, category, authorID}) => {
+        return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} category={category} />
+      })
+    }
+    </div> : <h2 className='center'>No posts yet</h2>} 
+</section>
   )
 }
 
