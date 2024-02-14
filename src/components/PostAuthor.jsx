bab22925bd370d1ae5e79324b387def7dedc700a
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 02:47:27 -0800

    Updates

---
+++ src/components/PostAuthor.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PostAuthor = () => {
+  return (
+    Link
+  )
+}
+
+export default PostAuthor
--- src/components/Posts.jsx
+++ src/components/Posts.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
+import React, {useState} from 'react'
 import Thumbnail1 from '../assets/blog1.jpg'
 import Thumbnail2 from '../assets/blog2.jpg'
 import Thumbnail3 from '../assets/blog3.jpg'
 import Thumbnail4 from '../assets/blog4.jpg'
-import Post from './Post'
+import Post from './PostItem'
 
 const dummyPosts = [
   {
@@ -17,28 +17,28 @@ const dummyPosts = [
     id: 2,
     thumbnail : Thumbnail2,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
-    category: 'education',
+    category: 'entertainment',
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
     authorID: 4,
   },{
     id: 3,
     thumbnail : Thumbnail3,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
-    category: 'education',
+    category: 'sports',
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
     authorID: 5,
   },{
     id: 4,
     thumbnail : Thumbnail4,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
-    category: 'education',
+    category: 'agriculture',
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
     authorID: 6,
   }, {
     id: 5,
     thumbnail : Thumbnail1,
     title: "Lorem ipsum dolor sit amet consectetur adipisicing." ,
-    category: 'education',
+    category: 'dance',
     desc: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate beatae facere repellat eligendi similique nihil fugit? Et, cum!",
     authorID: 3,
   },{
@@ -66,15 +66,16 @@ const dummyPosts = [
 ]
 
 const Posts = () => {
+  const [posts, setPosts] = useState(dummyPosts);
   return (
-    <section>
-      <div className="container posts">
+    <section className='posts'>
+        <div className="container posts__container">
         {
-          dummyPosts.map(({id, title, thumbnail, desc, authorID}) => {
-            return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} />
+          posts.map(({id, title, thumbnail, desc, category, authorID}) => {
+            return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} category={category} />
           })
         }
-      </div>
+        </div>
     </section>
   )
 }
