64a463651ac4b37a1e3c5d401b529b185ba0cbf5
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 02:53:36 -0800

    Updates

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import PostAuthor from './PostAuthor'
 
-const PostItem = ({id, title, thumbnail, desc, authorID}) => {
+const PostItem = ({id, title, thumbnail, desc, category, authorID}) => {
   return (
     <Link to={`posts/${id}`} className='post'>
       <div className="post__thumbnail">
@@ -9,11 +10,12 @@ const PostItem = ({id, title, thumbnail, desc, authorID}) => {
       </div>
       <div className="post__content">
         <Link to={`posts/${id}`}>
-          <h3>{title}</h3>
+          <h3>{title.length > 30 ? title.substr(0, 30) + "..." : title}</h3>
         </Link>
-        <p>{desc.length > 40 ? desc.substr(0, 40) + "..." : desc}</p>
+        <p>{desc.length > 145 ? desc.substr(0, 145) + "..." : desc}</p>
         <div className="post__footer">
-          
+          <PostAuthor />
+          <Link to={`post/categories/${category}`} className='btn category'>{category}</Link>
         </div>
       </div>
     </Link>
