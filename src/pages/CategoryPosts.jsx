b89a80bab4fce2d5f6e7a44f5781abebde016024
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 05:34:51 -0800

    Updates

--- src/pages/CategoryPosts.jsx
+++ src/pages/CategoryPosts.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
+import dummyPosts from '../data'
+import Post from '../components/PostItem'
 
-const CategoryPosts = () => {
+const AuthorPosts = () => {
+  const [posts, setPosts] = useState(dummyPosts)
   return (
-    <div>CategoryPosts</div>
+    <section className='author__posts'>
+    {posts.length > 0 ? <div className="container author__posts-container">
+    {
+      posts.map(({id, title, thumbnail, desc, category, authorID}) => {
+        return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} category={category} />
+      })
+    }
+    </div> : <h2 className='center'>No posts yet</h2>} 
+</section>
   )
 }
 
-export default CategoryPosts
+export default AuthorPosts
