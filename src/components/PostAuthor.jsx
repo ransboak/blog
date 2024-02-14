e78f3e9c128aa74e0178274cdc254164c319948c
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 02:57:22 -0800

    Updates

--- src/components/PostAuthor.jsx
+++ src/components/PostAuthor.jsx
@@ -4,12 +4,13 @@ import Avatar from '../assets/avatar1.jpg'
 
 const PostAuthor = () => {
   return (
-    <Link to={`post/users/bbshj`}>
+    <Link to={`post/users/bbshj`} className='post__author'>
         <div className="post__author-avatar">
             <img src={Avatar} alt="" />
         </div>
         <div className="post__author-details">
-            <h5>By: Ransford</h5>
+            <h5>By: Ransford Boakye</h5>
+            <small>Just now</small>
         </div>
 
     </Link>
