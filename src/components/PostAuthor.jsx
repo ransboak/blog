a7748eb9a068b9df2a9f49190a1bc9e41271821c
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 02:55:57 -0800

    Updates

--- src/components/PostAuthor.jsx
+++ src/components/PostAuthor.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import Avatar from '../assets/avatar1.jpg'
 
 const PostAuthor = () => {
   return (
-    Link
+    <Linkn to={`post/users/bbshj`}>
+        <div className="post__author-avatar">
+            <img src={Avatar} alt="" />
+        </div>
+
+    </Linkn>
   )
 }
 
