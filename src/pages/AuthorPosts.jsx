9a7be213d7c94d8f713b2b0a246a9de74a50569c
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-14 05:34:20 -0800

    Updates

--- src/pages/AuthorPosts.jsx
+++ src/pages/AuthorPosts.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 const AuthorPosts = () => {
   return (
-    <div>AuthorPosts</div>
+   <section className='posts'>
+        {posts.length > 0 ? <div className="container posts__container">
+        {
+          posts.map(({id, title, thumbnail, desc, category, authorID}) => {
+            return <Post id={id} title={title} thumbnail={thumbnail} desc={desc} authorID={authorID} category={category} />
+          })
+        }
+        </div> : <h2 className='center'>No posts yet</h2>} 
+    </section>
   )
 }
 
