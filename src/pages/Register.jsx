509a205d67827578b8d837e86b85f6fda2730690
ransboak <66210638+ransboak@users.noreply.github.com>
2024-02-16 04:20:47 -0800

    Worked on Registeration page

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  });
+
+  const changeInputHandler = (e) => {
+    setUserData(prevState => {
+      return {...prevState, [e.target.name] : e.target.value}
+    });
+  }
   return (
-    <div>Register</div>
+    <section className="register">
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form className="form register__form">
+          <p className="form__error-message">
+            This is an error message
+          </p>
+          <input type="text" value={userData.name} placeholder='Full Name' name='name' onChange={changeInputHandler} />
+          <input type="text" value={userData.email} placeholder='Email' name='email' onChange={changeInputHandler} />
+          <input type="text" value={userData.password} placeholder='Password' name='password' onChange={changeInputHandler} />
+          <input type="text" value={userData.password2} placeholder='Conform Password' name='password2' onChange={changeInputHandler} />
+          <button type="submit" className='btn primary'>Submit</button>
+        </form>
+        <small>Already have an account? <Link to='/login'>Sign In</Link></small>
+      </div>
+    </section>
   )
 }
 
